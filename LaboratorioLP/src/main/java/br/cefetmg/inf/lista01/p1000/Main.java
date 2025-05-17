d3515fd11f46c0248e200528c62fa8b09b18a363
Mateus0Vasconcelos <mateu@ComputadorMVRM>
2025-05-16 23:53:45 -0300

    Problemas 1000, 1001, 1003 beecrowd - 16/05/2025

---
+++ LaboratorioLP/src/main/java/br/cefetmg/inf/lista01/p1000/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.cefetmg.inf.lista01.p1000;
+
+/**
+ *
+ * @author mateu
+ */
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
