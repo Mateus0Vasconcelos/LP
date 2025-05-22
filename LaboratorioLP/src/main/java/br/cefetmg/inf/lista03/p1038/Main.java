7d1493da36370c5eec3a688f81b1cf4d3c42f55f
Mateus0Vasconcelos <mateu@ComputadorMVRM>
2025-05-22 20:10:06 -0300

    Problema beecrowd - 22/05/2025

--- LaboratorioLP/src/main/java/br/cefetmg/inf/lista03/p1038/Main.java
+++ LaboratorioLP/src/main/java/br/cefetmg/inf/lista03/p1038/Main.java
@@ -9,6 +9,7 @@ package br.cefetmg.inf.lista03.p1038;
  * @author aluno
  */
 import java.util.ArrayList;
+import java.util.Scanner;
 class Lanche {
     int codigo;
     String especificacao;
@@ -16,7 +17,7 @@ class Lanche {
 }
 class Cardapio {
     ArrayList<Lanche> lanches;
-    void setLanches() {
+    void inicializarLanches() {
         lanches = new ArrayList<>();
     }
     void addLanche(int codigo, String especificacao, double preco) {
@@ -26,19 +27,37 @@ class Cardapio {
         novoLanche.preco = preco;
         lanches.add(novoLanche);
     }
+    Lanche getLanche(int codigo) {
+        for (int i = 0; i < lanches.size(); i++) {
+            if (lanches.get(i).codigo == codigo)
+                return lanches.get(i);
+        }
+        return null;
+    }
 }
 class Venda {
     int codigo;
     int quantidade;
+    double operarVenda(Cardapio cardapio) {
+        double valorCompra = quantidade * cardapio.getLanche(codigo).preco;
+        System.out.printf("Total: R$ %.2f", valorCompra);
+        return valorCompra;
+    }
 }
 public class Main {
     public static void main(String args[]) {  
         Cardapio cardapio = new Cardapio();
-        cardapio.setLanches();
+        cardapio.inicializarLanches();
         cardapio.addLanche(1, "Cachorro Quente", 4.0);
         cardapio.addLanche(2, "X-Salada", 4.5);
         cardapio.addLanche(3, "X-Bacon", 5.0);
         cardapio.addLanche(4, "Torrada Simples", 2.0);
         cardapio.addLanche(5, "Refrigerante", 1.5);
+        Scanner scan = new Scanner(System.in);
+        Venda venda = new Venda();
+        venda.codigo = scan.nextInt();
+        venda.quantidade = scan.nextInt();
+        scan.close();
+        venda.operarVenda(cardapio); 
     }
 }
