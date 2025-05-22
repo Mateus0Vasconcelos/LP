353d245d34b9c90455963f0800f2c59972714ee4
Mateus0Vasconcelos <mateu@ComputadorMVRM>
2025-05-22 09:52:32 -0300

    Problema beecrowd - 18/05/2025

--- LaboratorioLP/src/main/java/br/cefetmg/inf/lista03/p1038/Main.java
+++ LaboratorioLP/src/main/java/br/cefetmg/inf/lista03/p1038/Main.java
@@ -13,35 +13,32 @@ class Lanche {
     int codigo;
     String especificacao;
     double preco;
-    double getValor(int quantidade) {
-        double preco = this.preco;
-        return quantidade * preco;
-    }
-    void setLanche(int codigo, String especificacao, double preco) {
-        this.codigo = codigo;
-        this.especificacao = especificacao;
-        this.preco = preco;
-    }
 }
 class Cardapio {
     ArrayList<Lanche> lanches;
     void setLanches() {
         lanches = new ArrayList<>();
     }
-    void add(int codigo, String especificacao, double preco) {
+    void addLanche(int codigo, String especificacao, double preco) {
         Lanche novoLanche = new Lanche();
-        novoLanche.setLanche(codigo, especificacao, preco);
+        novoLanche.codigo = codigo;
+        novoLanche.especificacao = especificacao;
+        novoLanche.preco = preco;
         lanches.add(novoLanche);
     }
+}
+class Venda {
+    int codigo;
+    int quantidade;
 }
 public class Main {
     public static void main(String args[]) {  
         Cardapio cardapio = new Cardapio();
         cardapio.setLanches();
-        cardapio.add(1, "Cachorro Quente", 4.0);
-        cardapio.add(2, "X-Salada", 4.5);
-        cardapio.add(3, "X-Bacon", 5.0);
-        cardapio.add(4, "Torrada Simples", 2.0);
-        cardapio.add(5, "Refrigerante", 1.5);
+        cardapio.addLanche(1, "Cachorro Quente", 4.0);
+        cardapio.addLanche(2, "X-Salada", 4.5);
+        cardapio.addLanche(3, "X-Bacon", 5.0);
+        cardapio.addLanche(4, "Torrada Simples", 2.0);
+        cardapio.addLanche(5, "Refrigerante", 1.5);
     }
 }
